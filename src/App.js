8b12745c364aa0705c3ae4623df3c9222d6a177e
codeoneline <mutouji1@163.com>
2021-03-05 16:45:20 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import * as sdk from './services/sdk';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [fee, setFee] = useState(0);
+  const [timestamp, setTimestamp] = useState(0);
+
+  const refreshFee = async function() {
+    const tmpFee = await sdk.getFee()
+    const tmpTS = await sdk.getTimeStamp()
+    setFee(tmpFee)
+    setTimestamp(tmpTS)
+  }
+
+  useEffect(() => {
+    let timer = setInterval(refreshFee, 10000);
+    return () => {
+      clearInterval(timer)
+    }
+  }, [fee, timestamp])
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          fee：{fee}
+        </div>
+        <div>
+          timestamp: {timestamp}
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
